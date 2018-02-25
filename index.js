01beded2027528e3d08769dc2436574c51f3e9fa
Aidarilyasov <06moboru@mail.ru>
2018-02-25 21:39:51 +0400

    new file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-var libraries = new XMLHttpRequest();
-libraries.open("GET", "colors.json", false);
+// var libraries = new XMLHttpRequest();
+// libraries.open("GET", "colors.json", false);
 
-libraries.send();
-if (libraries.status != 200) {
-  console.log( libraries.status + ': ' + libraries.statusText ); // пример вывода: 404: Not Found
-} else {
-  // вывести результат
- document.write( libraries.responseText ); // responseText -- текст ответа.
-}
+// libraries.send();
+// if (libraries.status != 200) {
+//   console.log( libraries.status + ': ' + libraries.statusText ); // пример вывода: 404: Not Found
+// } else {
+//   // вывести результат
+//  document.write( libraries.responseText ); // responseText -- текст ответа.
+// }
+
+fetch('./colors.json')
+  .then(function(response) {
+    console.log(response.headers.get('Content-Type')); // application/json; charset=utf-8
+    console.log(response.status); // 200
+
+    return response.json();
+   })
+  .then(function(user) {
+    console.log(user.name); // iliakan
+  })
+  .catch( alert );
